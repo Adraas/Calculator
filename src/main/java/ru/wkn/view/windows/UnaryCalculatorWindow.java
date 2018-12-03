e3981e990e8180e2c82d0b9e8227f313d43c176d
Adraas <pickalov.artyom@yandex.ru>
2018-12-03 21:52:44 +0400

    Some fix

--- src/main/java/ru/wkn/view/windows/UnaryCalculatorWindow.java
+++ src/main/java/ru/wkn/view/windows/UnaryCalculatorWindow.java
@@ -14,6 +14,7 @@ public class UnaryCalculatorWindow implements Window {
     public void windowInit(String windowName, double height, double width) throws IOException {
         AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/fxml/unary-calculator-window.fxml"));
         Stage stage = new Stage();
+
         stage.setTitle(windowName);
         stage.setScene(new Scene(anchorPane, width, height));
         stage.setResizable(false);
