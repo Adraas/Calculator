f8b9302c00353c30d42e199bb43b8724eb1cee11
Adraas <pickalov.artyom@yandex.ru>
2018-12-03 21:51:11 +0400

    The window's initialize was completed

--- src/main/java/ru/wkn/view/Window.java
+++ src/main/java/ru/wkn/view/Window.java
@@ -1,6 +1,8 @@
 package ru.wkn.view;
 
+import java.io.IOException;
+
 public interface Window {
 
-    void windowInit(String windowName, double height, double width);
+    void windowInit(String windowName, double height, double width) throws IOException;
 }
--- src/main/java/ru/wkn/view/windows/UnaryCalculatorWindow.java
+++ src/main/java/ru/wkn/view/windows/UnaryCalculatorWindow.java
@@ -1,10 +1,22 @@
 package ru.wkn.view.windows;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import ru.wkn.view.Window;
+
+import java.io.IOException;
 
 public class UnaryCalculatorWindow implements Window {
 
     @Override
-    public void windowInit(String windowName, double height, double width) {
+    public void windowInit(String windowName, double height, double width) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/fxml/unary-calculator-window.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle(windowName);
+        stage.setScene(new Scene(anchorPane, width, height));
+        stage.setResizable(false);
+        stage.show();
     }
 }
